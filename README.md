cea810528c31cebc3002696c26afe1e6957bbd91
vlad <martyanov@aeroidea.ru>
2022-10-09 18:26:54 +0300

    add currency triangulation

--- README.md
+++ README.md
@@ -3,9 +3,31 @@
 1) GET http://10.10.10.1:10000/rates?onDate=07.10.2022 - получение курсов валют, на дату из цб
 2) GET http://10.10.10.1:10000/exchange?from=USD&to=RUB&onDate=07.10.2022 получение курса на дату из базы
 
+## Работа с курсами
+ЦБ присылает 34 пары курсов валют. Из них получаются обратные курсы путем 1 / exchangeRate<br>
+После происходит триангуляция курсов путем sql запроса вида:
+
+```sql
+insert into currency_rates (currency_from, currency_to, created_at, on_date, exchange_rate)
+select pair.currency_from, pair.currency_to, pair.created_at, pair.on_date, pair.exchange_rate
+from (
+         select f.currency_from, t.currency_from as currency_to, f.created_at, f.on_date, (f.exchange_rate / t.exchange_rate) as exchange_rate
+         from currency_rates f, currency_rates t
+         where f.on_date = t.on_date
+           and f.currency_to = t.currency_to
+     ) pair
+         LEFT OUTER JOIN currency_rates cr
+                         ON (
+                                     pair.on_date = cr.on_date
+                                 AND pair.currency_from = cr.currency_from
+                                 AND pair.currency_to = cr.currency_to
+                             )
+where pair.on_date = 2022-10-07
+group by pair.currency_from, pair.currency_to
+```
+[Источник](http://www.dpxo.net/articles/fx_rate_triangulation_sql.html)
 
 ## todo:
-1) триангуляция курсов
-2) обмен валют
-3) докер
-4) постгрес
+1) обмен валют
+2) докер
+3) постгрес
--- app/internal/domain/use_case/update_exchanges/handler.go
+++ app/internal/domain/use_case/update_exchanges/handler.go
@@ -44,21 +44,6 @@ func (e UpdateExchangeHandler) ExchangeCbrRates(req *http.Request) {
 
 	log.Printf("Pairs was inserted to db")
 
-	//insert into currency_rates (currency_from, currency_to, created_at, on_date, exchange_rate)
-	//select pair.currency_from, pair.currency_to, pair.created_at, pair.on_date, pair.exchange_rate
-	//from (
-	//         select f.currency_from, t.currency_from as currency_to, f.created_at, f.on_date, (f.exchange_rate / t.exchange_rate) as exchange_rate
-	//         from currency_rates f, currency_rates t
-	//         where f.on_date = t.on_date
-	//           and f.currency_to = t.currency_to
-	//     ) pair
-	//         LEFT OUTER JOIN currency_rates cr
-	//                         ON (
-	//                                     pair.on_date = cr.on_date
-	//                                 AND pair.currency_from = cr.currency_from
-	//                                 AND pair.currency_to = cr.currency_to
-	//                             )
-	//group by pair.currency_from, pair.currency_to
 	e.db.Exec("insert into currency_rates (currency_from, currency_to, created_at, on_date, exchange_rate) "+
 		"select pair.currency_from, pair.currency_to, pair.created_at, pair.on_date, pair.exchange_rate "+
 		"from ("+
@@ -99,7 +84,7 @@ func processRate(cbrRate dto.CbrRate, onDate time.Time) RatePair {
 
 // Client request to CBR which return currency exchange rate list
 func getCbrRates(onDate time.Time) dto.CbrRates {
-	requestUrl := "https://cbr.ru/scripts/XML_daily.asp?date_req=" + onDate.Format("02/01/2006")
+	requestUrl := fmt.Sprintf("https://cbr.ru/scripts/XML_daily.asp?date_req=%s", onDate.Format("02/01/2006"))
 	resp, err := http.Get(requestUrl)
 	if err != nil {
 		log.Fatal(err)
